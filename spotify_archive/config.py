66c3419c3ca85e1934a095ea21f3d28366aa36e1
Fabian Stermann <fabian.stermann@gmail.com>
2022-07-22 23:47:41 +0200

    change config

--- spotify_archive/config.py
+++ spotify_archive/config.py
@@ -1,7 +1,10 @@
+import inspect
+import os
 from dataclasses import dataclass
 from typing import Dict
 
-import toml
+import yaml
+from dotenv import dotenv_values
 
 
 @dataclass
@@ -11,13 +14,36 @@ class PlaylistConfig:
 
 
 @dataclass
+class SpotifyConfig:
+    client_id: str
+    client_secret: str
+    redirect_uri: str
+    username: str
+    refresh_token: str
+
+    @classmethod
+    def from_env(cls, env):
+        return cls(
+            **{
+                k.lower(): v
+                for k, v in env.items()
+                if k.lower() in inspect.signature(cls).parameters
+            }
+        )
+
+
+@dataclass
 class Config:
     daily: Dict[str, PlaylistConfig]
     weekly: Dict[str, PlaylistConfig]
+    spotify: SpotifyConfig
 
 
-config_raw = toml.load("conf/config.toml")
+with open("conf/config.toml", "r") as f:
+    config_raw = yaml.safe_load(f)
+
 config = Config(
     daily={name: PlaylistConfig(**pc) for name, pc in config_raw["daily"].items()},
     weekly={name: PlaylistConfig(**pc) for name, pc in config_raw["weekly"].items()},
+    spotify=SpotifyConfig.from_env({**dotenv_values(".env"), **os.environ}),
 )
